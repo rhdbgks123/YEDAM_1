a400cec33fa85abb57ff774273119c5133723f5f
YUHAN KO <rhdbgks789@gmail.com>
2025-08-12 12:41:52 +0900

    [KYH] 250812 초기세팅

---
+++ YEDAM1/src/main/java/com/yedam1/common/FrontController.java
@@ -0,0 +1,44 @@
+package com.yedam1.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet
+{
+
+	Map<String, Control> map;
+
+	public FrontController()
+	{
+		map = new HashMap<String, Control>();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		//map.put("/boardList.do", new Control());
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		// url vs uri
+		// http://localhost:8080/HelloJSP/boardList.do - URL
+		// /HelloJSP/boardList.do - URI
+
+		String uri = req.getRequestURI();  
+		String context = req.getContextPath(); // /HelloJSP - 프로젝트
+		String page = uri.substring(context.length()); // context의 length부터 끝까지 자르기
+
+		Control control = map.get(page);
+		control.execute(req, res);
+	}
+
+}
