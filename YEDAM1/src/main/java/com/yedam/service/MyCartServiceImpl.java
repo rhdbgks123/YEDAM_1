01db89a7423d15800947c468d23765fefcbb65d2
Jeong JaeMin <jamin312@naver.com>
2025-08-13 17:28:04 +0900

    <JJM> 25.08.13 yedam 장바구니에 필요한 서비스Impl 생성

---
+++ YEDAM1/src/main/java/com/yedam/service/MyCartServiceImpl.java
@@ -0,0 +1,23 @@
+package com.yedam.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.yedam.common.DBUtil;
+import com.yedam.mapper.MyCartMapper;
+import com.yedam.vo.MyBasketVO;
+
+public class MyCartServiceImpl implements MyCartService{
+
+	SqlSession sqlSession = DBUtil.getInstance().openSession();
+	MyCartMapper mapper = sqlSession.getMapper(MyCartMapper.class);
+	
+	@Override
+	public List<MyBasketVO> cartList(String userId) {
+		return mapper.selectCartList(userId);
+	}
+	
+	
+	
+}
