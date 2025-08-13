421ccc7487766a2b42735cc70ba7e4d1e995987d
YUHAN KO <rhdbgks789@gmail.com>
2025-08-13 17:27:17 +0900

    [KYH] 250813 학원 작업분 추가

--- YEDAM1/src/main/java/com/yedam/service/UserService.java
+++ YEDAM1/src/main/java/com/yedam/service/UserService.java
@@ -5,4 +5,6 @@ import com.yedam.vo.UserVO;
 public interface UserService
 {
 	public UserVO userCheck(String id, String pw);
+	public int checkUserID(String id);
+	public boolean registerUser(UserVO user);
 }
