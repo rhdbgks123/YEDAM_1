9eed3ba280cf3a046c4456142dd06b6e6e6d268b
Jeong JaeMin <jamin312@naver.com>
2025-08-13 17:27:09 +0900

    <JJM> 25.08.13 yedam 장바구니 VO 생성

---
+++ YEDAM1/src/main/java/com/yedam/vo/MyBasketVO.java
@@ -0,0 +1,14 @@
+package com.yedam.vo;
+
+import lombok.Data;
+
+@Data
+public class MyBasketVO {
+	private String userId;
+	private String itemCode;
+	private int itemQty;
+	private String itemName;
+	private int salePrice;
+	private int price;
+	private int deliveryPrice;
+}
